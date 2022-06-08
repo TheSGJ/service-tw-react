36fcd108ce625b0ab436dbdb13604c1a6b6c22e9
Shri Ganesh Jha <realsgj@gmail.com>
2022-06-08 14:01:54 -0700

    Added Link react router

--- src/App.js
+++ src/App.js
@@ -4,45 +4,17 @@ import Footer from "./components/Footer";
 import Home from "./components/Home";
 import About from "./components/About";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import { Navbar } from 'responsive-navbar-react'
-import 'responsive-navbar-react/dist/index.css'
 import Contact from "./components/Contact";
 import { Container } from "@mui/material";
+import Navbar from "./components/Navbar";
 function App() {
-  const props = {
-    items: [
-      {
-        text: 'Home',
-        link: '/'
-      },
-      {
-        text: 'About',
-        link: '/about'
-      },
-      {
-        text: 'Contact',
-        link: '/contact'
-      }
-    ],
-    logo: {
-      text: 'Service - TW',
-      link: '/'
-    },
-    style: {
-      barStyles: {
-        background: '#171c24'
-      },
-      sidebarStyles: {
-        background: '#222',
-        buttonColor: 'white'
-      }
-    }
-  }
+
 
   return (
     <div className="dark:bg-gray-900">
-      <Navbar {...props}/>
+      
       <Router>
+      <Navbar />
           <Container>
           <Routes>
             <Route path="/" element={<Home />} />
--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -5,7 +5,7 @@ import { useForm, ValidationError } from '@formspree/react';
 const Contact = () => {
   const [state, handleSubmit] = useForm("mvolykjd");
   if (state.succeeded) {
-      return <p>Thanks for Feedback! We'll Get in Touch with you soon...</p>;
+      return <p>Thanks for your Feedback! We'll Get in Touch with you soon...</p>;
   }
     return ( 
     <div>
@@ -32,9 +32,10 @@ const Contact = () => {
                     id="email"
                     name="email"
                     className="w-full bg-gray-100 rounded border border-gray-300 focus:border-indigo-500 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out dark:bg-gray-700 dark:text-white dark:border-white"
+                    required
                   />
                   <ValidationError 
-                    prefix="Email" 
+                    prefix="Email"
                     field="email"
                     errors={state.errors}
                   />
@@ -52,6 +53,7 @@ const Contact = () => {
                     id="message"
                     name="message"
                     className="w-full bg-gray-100 rounded border border-gray-300 focus:border-indigo-500 h-32 text-base outline-none text-gray-700 py-1 px-3 resize-none leading-6 transition-colors duration-200 z-10 ease-in-out dark:bg-gray-700 dark:text-white dark:border-white"
+                    required
                   />
                   <ValidationError 
                     prefix="Message" 
